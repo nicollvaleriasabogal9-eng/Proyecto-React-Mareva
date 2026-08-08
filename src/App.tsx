350b2af204f38c5eec50b3d077bd2e7e4f134a30
laurasofiarubiano116-gif <laurasofiarubiano116@gmail.com>
2026-08-08 11:51:24 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,10 @@
-import Paquetes from './components/Paquetes';
-import Usuarios from './components/Usuarios';
-import './App.css'
-
+import Paquetes from "./components/Paquetes";
+import "./App.css";
 
 function App() {
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
-
-
-      <div style={{ display: 'flex', flex: 1 }}>
-        <Usuarios/>
-        <Paquetes />
-      </div>
-
-
+    <div>
+      <Paquetes />
     </div>
   );
 }
