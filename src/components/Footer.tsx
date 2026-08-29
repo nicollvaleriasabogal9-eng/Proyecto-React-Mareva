b3fb98dafef4233aafcce72df6e5c773c03ea43d
naamdvl <andresaroca721@gmail.com>
2026-08-29 10:52:47 -0500

    Actualizacion Mensaje Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -18,7 +18,7 @@ function Footer() {
       </div>
 
       <div className="footer-bottom">
-        © 2026 MAREVA · Viajes por Colombia Y todo el Mundo
+        © 2026 MAREVA · Viajes por Colombia 
       </div>
     </footer>
   );
